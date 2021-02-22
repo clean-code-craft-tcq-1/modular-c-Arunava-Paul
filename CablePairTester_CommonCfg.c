aa3940aa770ea0fed27eaff97cc3fa2f857fc835
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-02-22 23:57:31 +0530

    Add files via upload

---
+++ CablePairTester_CommonCfg.c
@@ -0,0 +1,24 @@
+/*
+ * CablePairTester_CommonCfg.c
+ *
+ *  Created on: Feb 22, 2021
+ *      Author: PUL1KOR
+ */
+
+
+#include <CablePairTester_CommonCfg.h>
+
+/**************
+ * Description : This will create string of the passed colour pairs
+ * In: ColorPair --> Structure pointer.
+ *     buffer    --> Output string buffer pointer.
+ *
+ * OUT: None
+ */
+void Fc_ColorPairToString(const ColorPair* colorPair, char* buffer)
+{
+	sprintf( buffer, "%s %s", 							\
+			 MajorColorNames[colorPair->majorColor], 	\
+			 MinorColorNames[colorPair->minorColor] 	\
+		   );
+}/*end of Fc_ColorPairToString*/

bc82f86ed71b500b566e25d6b146f216a19e5f69
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-02-23 02:09:31 +0530

    Update CableColourPair_Tetser.h

--- CableColourPair_Tetser.h
+++ CableColourPair_Tetser.h
@@ -8,7 +8,7 @@
 #ifndef CABLECOLOURPAIR_TETSER_H_
 #define CABLECOLOURPAIR_TETSER_H_
 
-extern void Fc_testNumberToPair(int pairNumber_int, enum MajorColor_ten expectedMajor_en, enum MinorColor_ten expectedMinor_en);
-extern ColorPair_tst Fc_GetColorFromPairNumber(int pairNumber_int);
+void Fc_testNumberToPair(int pairNumber_int, enum MajorColor_ten expectedMajor_en, enum MinorColor_ten expectedMinor_en);
+ColorPair_tst Fc_GetColorFromPairNumber(int pairNumber_int);
 
 #endif /* CABLECOLOURPAIR_TETSER_H_ */

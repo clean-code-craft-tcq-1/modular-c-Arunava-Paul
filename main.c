2ba918969ae0d957ac412d5efff5c90dda9d28fe
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-02-25 04:55:12 +0530

    Update main.c
    intermediate version. Comments regarding the file i will update

--- main.c
+++ main.c
@@ -1,37 +1,40 @@
 /*
  * main.c
- *
  *  Created on: Feb 22, 2021
  *      Author: PUL1KOR
  */
-
-
 #include <stdio.h>
 #include "CablePairTester_CommonCfg.h"
-#include "CablePairNumber_Tetser.h"
-#include "CableColourPair_Tetser.h"
-
+#include "CablePairNumber_Tester.h"
+#include "CableColourPair_Tester.h"
 /********************
- *
  * Des: Main function. This will call the Colour pair testing functions.
  * Input : None
  * Output: None
- *
  */
-
 int main()
 {
-	/*********
-	 * available major colours :  WHITE, RED, BLACK, YELLOW, VIOLET
-	 * available minor colours :  BLUE, ORANGE, GREEN, BROWN, SLATE
-	 * other colours are invalid. Passing will return error.
-	 * "https://en.wikipedia.org/wiki/25-pair_color_code" fore more details
-	 */
-    Fc_testNumberToPair(4, WHITE, BROWN);
-    Fc_testNumberToPair(5, WHITE, SLATE);
-    Fc_testPairToNumber(BLACK, ORANGE, 12);
-    Fc_testPairToNumber(VIOLET, SLATE, 25);
-	int c;
-	scanf("%d",&c);
+	char key_ip;
+	printf("-------Telecom Cable pair tester code---------\n\n"
+			"available major colours :  WHITE, RED, BLACK, YELLOW, VIOLET \n available minor colours :  BLUE, ORANGE, GREEN, BROWN, SLATE\n"
+			"For reference table press 'a' otherwise to test combination press 'b'\n\n ");
+	while(!((key_ip== 'a') || (key_ip== 'b')))
+		scanf("%c",&key_ip);/*Scan the character until the user press correct data*/
+	key_ip == 'a'? Fc_Reference_Table():Fc_Test_pairCombo();
+//    Fc_testNumberToPair(4, WHITE, BROWN);
+//    Fc_testNumberToPair(5, WHITE, SLATE);
+//    Fc_testPairToNumber(BLACK, ORANGE, 12);
+//    Fc_testPairToNumber(VIOLET, SLATE, 25);
     return 0;
+}/*end of main*/
+
+void Fc_Test_pairCombo(void)
+{
+	char key_ip;
+	int MjC_int,MnC_int,Pair;
+	printf("---Cable pair tester--\n\n To test the pair number to Colour combo press 'a' , To test the colour combo to pair number press 'b'\n\n ");
+	while(!((key_ip== 'a') || (key_ip== 'b')))
+			scanf("%c",&key_ip);/*Scan the character until the user press correct data*/
+	printf("Please ");
+	key_ip == 'a'? Fc_testPairToNumber():Fc_testNumberToPair();
 }

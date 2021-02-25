927c1e3a32e2dcb4e95b4fe71ba3d9eb4428c043
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-02-25 19:58:25 +0530

    Update CablePairTester_CommonCfg.c

--- CablePairTester_CommonCfg.c
+++ CablePairTester_CommonCfg.c
@@ -48,8 +48,10 @@ void Fc_Reference_Table(void)
 	printf("*******End of Reference Table ********** \n");
 }/*end of Fc_Reference_Table*/
 
-void Fc_UserInput_Handler(int *cable_pair_no,int *major_colour_en_no,int *minor_colour_en_no )
+void Fc_UserInput_Handler(int *cable_pair_no,int *major_colour_en_no,int *minor_colour_en_no,int choice_int )
 {
+#if(USER_INPUT_MODE_ACTIVALED == TRUE)
+	    (void)(choice_int);// not used
 		printf("\n---Colour Combo to pair tester---\n\n According to pair number standard 1-25 is available.\n"
 			   "The numbers corresponding to the colours are as followed : \n  WHITE = 1, RED= 2, BLACK=3, YELLOW=4, VIOLET=5, BLUE=6, ORANGE=7, GREEN=8, BROWN=9, SLATE=10\n");
 		printf("\nPlease enter the pair no:");
@@ -60,4 +62,35 @@ void Fc_UserInput_Handler(int *cable_pair_no,int *major_colour_en_no,int *minor_
 		while(!(((*major_colour_en_no <= 5) && (*major_colour_en_no >= 1)) && ((*minor_colour_en_no <= 10) && (*minor_colour_en_no >= 6))))
 		        scanf("%d %d",major_colour_en_no,minor_colour_en_no);/*scan till the user give proper reference number*/
 		printf("\nYou have entered : major_colour %s , minor_colour %s\n",MajorColorNames[(*major_colour_en_no) - MAJOR_COLOUR_REFERENCE_NO_OFFSET],MinorColorNames[(*minor_colour_en_no) - MINOR_COLOUR_REFERENCE_NO_OFFSET]);
-}
+#else
+	/*keeping the same case as original files*/
+	switch(choice_int)
+	{
+	case 1:
+		*cable_pair_no = 4 ;
+		*major_colour_en_no = 1;
+		*minor_colour_en_no = 9 ;
+		break;
+	case 2:
+		*cable_pair_no = 5;
+		*major_colour_en_no = 1;
+		*minor_colour_en_no = 10;
+		break;
+	case 3:
+		*cable_pair_no = 12;
+		*major_colour_en_no = 3;
+		*minor_colour_en_no = 7;
+		break;
+	case 4:
+		*cable_pair_no =15 ;
+		*major_colour_en_no = 5;
+		*minor_colour_en_no = 10;
+		break;
+	default:
+		*cable_pair_no =15 ;
+		*major_colour_en_no = 5;
+		*minor_colour_en_no = 10;
+		break;
+	}
+#endif
+}/*end of Fc_UserInput_Handler*/

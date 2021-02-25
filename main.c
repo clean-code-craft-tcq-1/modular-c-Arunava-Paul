6ec42d6eb837da0d77128076f260d9ff95d17e8f
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-02-25 19:59:03 +0530

    Update main.c
    Added the reference table function

--- main.c
+++ main.c
@@ -18,22 +18,30 @@ int main()
 	printf("-------Telecom Cable pair tester code---------\n\n"
 			"available major colours :  WHITE, RED, BLACK, YELLOW, VIOLET \n available minor colours :  BLUE, ORANGE, GREEN, BROWN, SLATE\n"
 			"For reference table press 'a' otherwise to test combination press 'b'\n\n ");
+#if(USER_INPUT_MODE_ACTIVALED == TRUE)
 	while(!((key_ip== 'a') || (key_ip== 'b')))
 		scanf("%c",&key_ip);/*Scan the character until the user press correct data*/
 	key_ip == 'a'? Fc_Reference_Table():Fc_Test_pairCombo();
-//    Fc_testNumberToPair(4, WHITE, BROWN);
-//    Fc_testNumberToPair(5, WHITE, SLATE);
-//    Fc_testPairToNumber(BLACK, ORANGE, 12);
-//    Fc_testPairToNumber(VIOLET, SLATE, 25);
+#else
+	/**Since Github does not support SCANF run manual function calling has been configured**/
+	Fc_Reference_Table();
+	Fc_Test_pairCombo();
+#endif
     return 0;
 }/*end of main*/
 
+/*This function will trigger the pair no to colour or colour to pair tester function**/
 void Fc_Test_pairCombo(void)
 {
 	char key_ip;
 	printf("---Cable pair tester--\n\n To test the pair number to Colour combo press 'a' , To test the colour combo to pair number press 'b'\n\n ");
+#if(USER_INPUT_MODE_ACTIVALED == TRUE)
 	while(!((key_ip== 'a') || (key_ip== 'b')))
 			scanf("%c",&key_ip);/*Scan the character until the user press correct data*/
 	printf("Please ");
 	key_ip == 'a'? Fc_testPairToNumber():Fc_testNumberToPair();
-}
+#else
+	Fc_testPairToNumber();
+	Fc_testNumberToPair();
+#endif
+}/*end of Fc_Test_pairCombo*/
